6cb2a521f1590f5e3bb2be82ad1c62a44564608f
gigabotan <aaazat7@gmail.com>
2025-12-08 00:08:04 +0100

    feat: solved 4.Arrays - HistogramTask

--- 4.Arrays/Names/HistogramTask.cs
+++ 4.Arrays/Names/HistogramTask.cs
@@ -4,9 +4,22 @@ internal static class HistogramTask
 {
     public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
     {
+        var days = new double[31];
+        foreach (var n in names)
+        {
+            if (n.Name == name && n.BirthDate.Day != 1)
+            {
+                days[n.BirthDate.Day - 1]++;
+            }
+        }
+
+        var dayLabels = new string[31];
+        for (var i = 0; i < 31; i++)
+            dayLabels[i] = (i + 1).ToString();
+
         return new HistogramData(
             $"Рождаемость людей с именем '{name}'", 
-            new [] {"1"}, 
-            new[] {0d});
+            dayLabels, 
+            days);
     }
 }
