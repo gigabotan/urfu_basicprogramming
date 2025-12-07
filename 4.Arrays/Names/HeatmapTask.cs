3c0a84b906e09c787440f4d4da895f3dd4db4ffd
gigabotan <aaazat7@gmail.com>
2025-12-08 00:24:11 +0100

    feat: solved 4.Arrays - HeatmapTask

--- 4.Arrays/Names/HeatmapTask.cs
+++ 4.Arrays/Names/HeatmapTask.cs
@@ -4,10 +4,27 @@ internal static class HeatmapTask
 {
     public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
     {
-        return new HeatmapData(
-            "Пример карты интенсивностей",
-            new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
-            new[] { "a", "b", "c", "d" }, 
-            new[] { "X", "Y", "Z" });
+        var heat = new double[30, 12];
+        for (var i = 0; i < names.Length; i++)
+        {
+            var birthdate = names[i].BirthDate;
+            if (birthdate.Day == 1)
+                continue;
+            heat[birthdate.Day - 2, birthdate.Month - 1]++;
+        }
+
+        var xLabels = new string[30];
+        for (var i = 0; i < 30; i++)
+        {
+            xLabels[i] = (i + 2).ToString();
+        }
+
+        var yLabels = new string[12];
+        for (var i = 0; i < 12; i++)
+        {
+            yLabels[i] = (i + 1).ToString();
+        }
+
+        return new HeatmapData("Карта интенсивностей рождаемости", heat, xLabels, yLabels);
     }
 }
