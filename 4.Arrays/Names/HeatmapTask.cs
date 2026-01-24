3120ac0d6ea0058d92d35a3a0deaee400a6f278f
gigabotan <aaazat7@gmail.com>
2026-01-24 16:46:52 +0100

    fix: Arrays - HeatmapTask, HistogramTask code review

--- 4.Arrays/Names/HeatmapTask.cs
+++ 4.Arrays/Names/HeatmapTask.cs
@@ -13,18 +13,19 @@ internal static class HeatmapTask
             heat[birthdate.Day - 2, birthdate.Month - 1]++;
         }
 
-        var xLabels = new string[30];
-        for (var i = 0; i < 30; i++)
-        {
-            xLabels[i] = (i + 2).ToString();
-        }
-
-        var yLabels = new string[12];
-        for (var i = 0; i < 12; i++)
-        {
-            yLabels[i] = (i + 1).ToString();
-        }
+        var xLabels = GenerateNumberArray(30, 2);
+        var yLabels = GenerateNumberArray(12, 1);
 
         return new HeatmapData("Карта интенсивностей рождаемости", heat, xLabels, yLabels);
+    }
+
+    private static string[] GenerateNumberArray(int count, int startValue)
+    {
+        var array = new string[count];
+        for (var i = 0; i < count; i++)
+        {
+            array[i] = (i + startValue).ToString();
+        }
+        return array;
     }
 }
--- 4.Arrays/Names/HistogramTask.cs
+++ 4.Arrays/Names/HistogramTask.cs
@@ -4,22 +4,28 @@ internal static class HistogramTask
 {
     public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
     {
-        var days = new double[31];
-        foreach (var n in names)
+        var birthCounts = new double[31];
+        foreach (var person in names)
         {
-            if (n.Name == name && n.BirthDate.Day != 1)
+            if (person.Name == name && person.BirthDate.Day != 1)
             {
-                days[n.BirthDate.Day - 1]++;
+                birthCounts[person.BirthDate.Day - 1]++;
             }
         }
 
+        var dayLabels = GenerateDayLabels();
+
+        return new HistogramData(
+            $"Рождаемость людей с именем '{name}'",
+            dayLabels,
+            birthCounts);
+    }
+
+    private static string[] GenerateDayLabels()
+    {
         var dayLabels = new string[31];
         for (var i = 0; i < 31; i++)
             dayLabels[i] = (i + 1).ToString();
-
-        return new HistogramData(
-            $"Рождаемость людей с именем '{name}'", 
-            dayLabels, 
-            days);
+        return dayLabels;
     }
 }
