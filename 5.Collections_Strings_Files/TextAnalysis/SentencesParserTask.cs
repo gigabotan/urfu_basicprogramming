65e4e22327487637ae94e72719af71ab7da7803f
gigabotan <aaazat7@gmail.com>
2026-01-24 20:55:35 +0100

    feat: add Chapter 5 solutions

--- 5.Collections_Strings_Files/TextAnalysis/SentencesParserTask.cs
+++ 5.Collections_Strings_Files/TextAnalysis/SentencesParserTask.cs
@@ -5,7 +5,42 @@ static class SentencesParserTask
     public static List<List<string>> ParseSentences(string text)
     {
         var sentencesList = new List<List<string>>();
-        //...
+
+        var sentenceDelimiters = new char[] { '.', '!', '?', ';', ':', '(', ')' };
+        var sentences = text.Split(sentenceDelimiters);
+
+        foreach (var sentence in sentences)
+        {
+            var words = new List<string>();
+            var currentWord = new System.Text.StringBuilder();
+
+            foreach (var ch in sentence)
+            {
+                if (char.IsLetter(ch) || ch == '\'')
+                {
+                    currentWord.Append(char.ToLower(ch));
+                }
+                else
+                {
+                    if (currentWord.Length > 0)
+                    {
+                        words.Add(currentWord.ToString());
+                        currentWord.Clear();
+                    }
+                }
+            }
+
+            if (currentWord.Length > 0)
+            {
+                words.Add(currentWord.ToString());
+            }
+
+            if (words.Count > 0)
+            {
+                sentencesList.Add(words);
+            }
+        }
+
         return sentencesList;
     }
 }
--- 5.Collections_Strings_Files/TextAnalysis/TextGeneratorTask.cs
+++ 5.Collections_Strings_Files/TextAnalysis/TextGeneratorTask.cs
@@ -7,6 +7,32 @@ static class TextGeneratorTask
         string phraseBeginning,
         int wordsCount)
     {
-        return phraseBeginning;
+        var words = phraseBeginning.Split(' ').ToList();
+
+        for (int i = 0; i < wordsCount; i++)
+        {
+            var nextWord = GetNextWord(nextWords, words);
+            if (nextWord == null)
+                break;
+            words.Add(nextWord);
+        }
+
+        return string.Join(" ", words);
+    }
+
+    private static string GetNextWord(Dictionary<string, string> nextWords, List<string> words)
+    {
+        if (words.Count >= 2)
+        {
+            var bigramKey = words[words.Count - 2] + " " + words[words.Count - 1];
+            if (nextWords.TryGetValue(bigramKey, out var nextWord))
+                return nextWord;
+        }
+
+        var unigramKey = words[words.Count - 1];
+        if (nextWords.TryGetValue(unigramKey, out var word))
+            return word;
+
+        return null;
     }
 }
