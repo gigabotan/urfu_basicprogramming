bd0ebd88ca09391d0874af907f51c94e8b39dd4a
gigabotan <aaazat7@gmail.com>
2026-01-24 16:01:49 +0100

    fix: EmptyMazeTask code review

--- 3.Loops/Mazes/EmptyMazeTask.cs
+++ 3.Loops/Mazes/EmptyMazeTask.cs
@@ -2,17 +2,17 @@
 
 public static class EmptyMazeTask
 {
-	public static void MoveToDirection(Robot robot, int steps, Direction direction)
-	{
-		for (var i = 0; i < steps; i++)
-			robot.MoveTo(direction);
-	}
+    public static void MoveOut(Robot robot, int width, int height)
+    {
+        var stepsRight = width - 3;
+        var stepsDown = height - 3;
+        MoveToDirection(robot, stepsRight, Direction.Right);
+        MoveToDirection(robot, stepsDown, Direction.Down);
+    }
 
-	public static void MoveOut(Robot robot, int width, int height)
-	{
-		var stepsRight = width - 3;
-		var stepsDown = height - 3;
-		MoveToDirection(robot, stepsRight, Direction.Right);
-		MoveToDirection(robot, stepsDown, Direction.Down);
-	}
+    private static void MoveToDirection(Robot robot, int stepCount, Direction direction)
+    {
+        for (var i = 0; i < stepCount; i++)
+            robot.MoveTo(direction);
+    }
 }
