741af58613997aa222d44684567199bc3bf3242a
gigabotan <aaazat7@gmail.com>
2026-01-26 00:33:09 +0100

    fix: code review for chapter 6

--- 6.Testing/TableParser/FieldsParserTask.cs
+++ 6.Testing/TableParser/FieldsParserTask.cs
@@ -11,27 +11,45 @@ public class FieldParserTaskTests
     {
         var actualResult = FieldsParserTask.ParseLine(input);
         NUnit.Framework.Legacy.ClassicAssert.AreEqual(expectedResult.Length, actualResult.Count);
-        for (int i = 0; i < expectedResult.Length; ++i)
+        for (var i = 0; i < expectedResult.Length; ++i)
         {
             NUnit.Framework.Legacy.ClassicAssert.AreEqual(expectedResult[i], actualResult[i].Value);
         }
     }
 
+    // Простое слово без пробелов
+    [TestCase("text", new[] { "text" })]
+    // Два слова, разделенные пробелом
     [TestCase("hello world", new[] { "hello", "world" })]
+    // Множественные пробелы между словами
     [TestCase("a    b", new[] { "a", "b" })]
+    // Пустая строка
     [TestCase("", new string[] { })]
+    // Только пробелы
     [TestCase("   ", new string[] { })]
+    // Пустая строка в одинарных кавычках
     [TestCase("''", new[] { "" })]
+    // Текст с пробелами в одинарных кавычках
     [TestCase("'hello world'", new[] { "hello world" })]
+    // Двойные кавычки, содержащие одинарные кавычки
     [TestCase("\"a 'b' c\"", new[] { "a 'b' c" })]
+    // Одинарные кавычки, содержащие двойные кавычки
     [TestCase("'a \"b\" c'", new[] { "a \"b\" c" })]
+    // Слово, за которым следует текст в кавычках без пробела
     [TestCase("a\"b c\"", new[] { "a", "b c" })]
+    // Текст в кавычках, за которым следует слово без пробела
     [TestCase("\"b c\"d", new[] { "b c", "d" })]
+    // Незакрытая двойная кавычка
     [TestCase("\"abc", new[] { "abc" })]
+    // Незакрытая двойная кавычка с пробелами
     [TestCase("\"a b ", new[] { "a b " })]
+    // Экранированные двойные кавычки внутри двойных кавычек
     [TestCase("\"a \\\"c\\\"\"", new[] { "a \"c\"" })]
+    // Экранированные одинарные кавычки внутри одинарных кавычек
     [TestCase("'a \\'c\\''", new[] { "a 'c'" })]
+    // Экранированный обратный слеш в кавычках
     [TestCase("\"\\\\\"", new[] { "\\" })]
+    // Обратные слеши вне кавычек
     [TestCase("\\\\", new[] { "\\\\" })]
     public static void RunTests(string input, string[] expectedOutput)
     {
@@ -44,7 +62,7 @@ public class FieldsParserTask
     public static List<Token> ParseLine(string line)
     {
         var tokens = new List<Token>();
-        int index = 0;
+        var index = 0;
         while (index < line.Length)
         {
             if (char.IsWhiteSpace(line[index]))
@@ -68,14 +86,14 @@ public class FieldsParserTask
 
     private static Token ReadSimpleField(string line, int startIndex)
     {
-        int index = startIndex;
+        var index = startIndex;
         while (index < line.Length && !char.IsWhiteSpace(line[index])
             && line[index] != '"' && line[index] != '\'')
         {
             index++;
         }
-        int length = index - startIndex;
-        string value = line.Substring(startIndex, length);
+        var length = index - startIndex;
+        var value = line.Substring(startIndex, length);
         return new Token(value, startIndex, length);
     }
 
